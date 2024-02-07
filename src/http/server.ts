a788ca661017852207e39e73e0089665b9f36f76
flvSantos15 <flvSantos300@gmail.com>
2024-02-07 00:42:39 -0300

    chore: initial commit

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,25 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello NLW'
+const prisma = new PrismaClient()
+
+app.post('/polls', async (req, res) => {
+  const cretePollBody = z.object({
+    title: z.string()
+  })
+
+  const { title } = cretePollBody.parse(req.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title
+    }
+  })
+
+  return res.status(201).send({ pollId: poll.id })
 })
 
 app.listen({ port: 3333 }).then(() => {
